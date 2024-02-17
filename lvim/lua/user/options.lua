07509203f540930cd983b467e824438168685de8
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-02-17 16:34:56 +0200

    Opened files are not folded by default

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -4,6 +4,7 @@ vim.opt.shiftwidth = 2            -- the number of spaces inserted for each inde
 vim.opt.tabstop = 2               -- insert 2 spaces for a tab
 vim.opt.relativenumber = true     -- relative line numbers
 vim.opt.wrap = true               -- wrap lines
+vim.opt.foldlevel = 99
 
 -- Show trailing whitespaces
 -- vim.opt.listchars = { eol = '↵' }
@@ -11,8 +12,10 @@ vim.opt.list = true
 
 vim.opt.termguicolors = true
 
---
 -- add gt and gT to go to previous/next tab inside lvim
 -- requires the bufferline plugin
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+
+-- configure telescope find window file to be bigger
+lvim.builtin.telescope.defaults.layout_config.width = 0.75
